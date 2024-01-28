ab4f517ef311c2e099d71871e361c784a67a4b44
ankush <139459126+paimenter@users.noreply.github.com>
2024-01-29 00:56:44 +0530

    Add files via upload

---
+++ projrct/patten.java
@@ -0,0 +1,12 @@
+public class patten{
+    public static void  printpatten ( ){
+        int i,j,n=6;
+        for(i =1; i<=n; i++)
+        {
+            for(j=1;j<=1;j++)
+System.out.print("*");
+        }
+        System.out.print(" ");
+    }
+
+}
